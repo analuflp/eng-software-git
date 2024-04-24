f65a664b788a380ad553c8a9e5cfd44831f4744a
analuflp <ana.felipe@dcx.ufpb.br>
2024-04-24 09:33:32 -0300

    -adicionando soma

---
+++ atvGit/src/Soma.java
@@ -0,0 +1,21 @@
+public class Soma {
+
+    private int numero1;
+    private int numero2;
+    private int resultado;
+
+
+    public void somarDoisNumeros(int num1, int num2) {
+        this.numero1 = num1;
+        this.numero2 = num2;
+        this.resultado = num1 + num2;
+    }
+
+
+    public int getResultado() {
+        return resultado;
+    }
+
+
+
+}
--- atvGit/src/atividadeEngSoftware.java
+++ atvGit/src/atividadeEngSoftware.java
@@ -1,6 +1,17 @@
 public class atividadeEngSoftware {
+
     public static void main(String[] args) {
 
-        System.out.println("Ana c:");
+        Soma soma = new Soma();
+
+
+        int valor1 = 10;
+        int valor2 = 10;
+
+
+        soma.somarDoisNumeros(valor1, valor2);
+
+        int resultadoSoma = soma.getResultado();
+        System.out.println("Meu nome é Ana e a soma de " + valor1 + " e " + valor2 + " é " + resultadoSoma);
     }
 }
